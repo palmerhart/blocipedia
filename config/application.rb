050be4d5b9d0b3434d449191290a309f42fcfcda
Palmer Hart <palmerhart@gmail.com>
2017-03-10 15:00:43 +0000

    blocipedia-5: partial for initial heroku push

--- config/application.rb
+++ config/application.rb
@@ -40,7 +40,7 @@ module Blocipedia
       :domain   => 'mail.google.com',
       :port     => 587,
       :user_name  => "palmerhart@gmail.com",
-      :password => "password",
+      :password => "nJDbofHUyJoP",
       :enable_starttls_auto => true
     }
     
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -43,13 +43,5 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: 'https://blocipedia-palmtree135.c9users.io/' }
   config.action_mailer.delivery_method = :smtp
   
-  ActionMailer::Base.smtp_settings = {
-      :address  => 'smtp.gmail.com',
-      :domain   => 'mail.google.com',
-      :port     => 587,
-      :user_name  => "palmerhart@gmail.com",
-      :password => "password",
-      :enable_starttls_auto => true
-    }
     
 end
