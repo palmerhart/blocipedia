4227e2dae8eb6a45d5b47e9e311158230608ee9a
Palmer Hart <palmerhart@gmail.com>
2017-04-22 16:31:04 +0000

    unable to update collaborators

--- app/models/collaborator.rb
+++ app/models/collaborator.rb
@@ -3,6 +3,15 @@ class Collaborator < ActiveRecord::Base
     belongs_to :wiki
     
     
+    #method to update collaborators via checkboxes on edit record of wiki - Has Many Through relationship
+    def self.update_collaborators(collaborator, params)
+        attributes = params.require(:user).permit( { wiki_id: [] })
+            user.wiki_id = attributes[:wiki_id].select{ |x| x.to_i > 0}
+            user.save
+        
+        !user.errors.present?
+    end
+   
     def self.wikis
         Wiki.where( id: pluck(:wiki_id))
     end
